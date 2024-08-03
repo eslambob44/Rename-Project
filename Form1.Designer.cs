13cfe4bb6079588672866bd32e11ddc1bbe440d4
eslambob44 <eyasser237@gmail.com>
2024-08-03 17:42:48 +0300

    Making The Project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,116 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtOldName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewName = new System.Windows.Forms.TextBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnRenameFiles = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Open File";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // txtOldName
+            // 
+            this.txtOldName.Enabled = false;
+            this.txtOldName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOldName.Location = new System.Drawing.Point(119, 244);
+            this.txtOldName.Name = "txtOldName";
+            this.txtOldName.Size = new System.Drawing.Size(214, 26);
+            this.txtOldName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The name to replace";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(644, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "New name";
+            // 
+            // txtNewName
+            // 
+            this.txtNewName.Enabled = false;
+            this.txtNewName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewName.Location = new System.Drawing.Point(589, 244);
+            this.txtNewName.Name = "txtNewName";
+            this.txtNewName.Size = new System.Drawing.Size(214, 26);
+            this.txtNewName.TabIndex = 3;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFolder.Location = new System.Drawing.Point(378, 94);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(163, 57);
+            this.btnOpenFolder.TabIndex = 4;
+            this.btnOpenFolder.Text = "Open Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnRenameFiles
+            // 
+            this.btnRenameFiles.Enabled = false;
+            this.btnRenameFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRenameFiles.Location = new System.Drawing.Point(378, 323);
+            this.btnRenameFiles.Name = "btnRenameFiles";
+            this.btnRenameFiles.Size = new System.Drawing.Size(163, 57);
+            this.btnRenameFiles.TabIndex = 5;
+            this.btnRenameFiles.Text = "Rename Files";
+            this.btnRenameFiles.UseVisualStyleBackColor = true;
+            this.btnRenameFiles.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(922, 513);
+            this.Controls.Add(this.btnRenameFiles);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.txtNewName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOldName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtOldName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewName;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnRenameFiles;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Rename_Project
 {
@@ -15,6 +17,87 @@ namespace Rename_Project
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtOldName.Enabled = true;
+                txtNewName.Enabled = true;
+                btnRenameFiles.Enabled = true;
+            }
+        }
+
+        
+
+        void RenameFilesInSpecificFolder(string FolderDir , string ReplacedName , string NameToReplace = "")
+        {
+            int Counter = 1;
+            string[] FilesNames = Directory.GetFiles(FolderDir);
+            foreach(string FileName in FilesNames)
+            {
+                string OldName = Path.GetFileNameWithoutExtension(FileName);
+                string NewName = (NameToReplace=="")? ReplacedName+" "+Counter: OldName.Replace(NameToReplace,ReplacedName);
+                string NewDir = Path.Combine(FolderDir,NewName+Path.GetExtension(FileName));
+                File.Move(FileName, NewDir);
+                Counter++;
+            }
+        }
+
+        bool CheckIfFolderHaveSubFolders(string FolderDir)
+        {
+            string[] FolderDirs = Directory.GetDirectories(FolderDir);
+            return (FolderDirs.Length > 0);
+        }
+
+        void RenameFilesInSpecificFolderAndSubFolders(string FolderDir, string ReplacedName, string NameToReplace = "")
+        {
+            string[] FoldersDir = Directory.GetDirectories(FolderDir);
+            string[]FilesDir = Directory.GetFiles(FolderDir);
+            RenameFilesInSpecificFolder(FolderDir, ReplacedName, NameToReplace);
+            foreach (string FolderName in FoldersDir)
+            {
+                RenameFilesInSpecificFolderAndSubFolders(FolderName , ReplacedName, NameToReplace);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtNewName.Text))
+            {
+                errorProvider1.SetError(txtNewName, "Must enter new name");
+                return;
+            }
+
+            if(CheckIfFolderHaveSubFolders(folderBrowserDialog1.SelectedPath))
+            {
+
+                if(MessageBox.Show("Are You Want To Change Files Name In SubFolders Inside This Folder",""
+                    ,MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+                {
+                    RenameFilesInSpecificFolderAndSubFolders(folderBrowserDialog1.SelectedPath, txtNewName.Text, txtOldName.Text);
+                }
+            }
+            else
+            {
+                RenameFilesInSpecificFolder(folderBrowserDialog1.SelectedPath, txtNewName.Text, txtOldName.Text);
+
+            }
+            MessageBox.Show("Names replaced Successfully");
+
+
+
+
+
+
+            txtOldName.Enabled = false;
+            txtNewName.Enabled = false;
+            btnRenameFiles.Enabled = false;
+            txtOldName.Text = "";
+            txtNewName.Clear(); 
+
         }
     }
 }
